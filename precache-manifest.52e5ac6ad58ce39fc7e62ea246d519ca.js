e632288633e1d7207505407b0bd1e6787d384f74
Roshan Kumar <roshankunaguero09@gmail.com>
2019-12-15 15:25:37 +0530

    initial-commit

---
+++ precache-manifest.52e5ac6ad58ce39fc7e62ea246d519ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac49acb199e7a9ee3bb08b2e7d89b4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcc0ed2cf1a1f1e7ae27",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d91e6a44e1930ea619a9",
+    "url": "/static/js/2.f1ac62f9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.f1ac62f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bcc0ed2cf1a1f1e7ae27",
+    "url": "/static/js/main.15e75bbb.chunk.js"
+  },
+  {
+    "revision": "d8b9ed13bdc86bfe9f58",
+    "url": "/static/js/runtime-main.1fd89717.js"
+  }
+]);
